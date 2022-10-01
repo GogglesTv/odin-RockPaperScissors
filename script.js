f13cc614f85298632af6df82e0c688d5b95f02c0
Jalen Moore <jalenmarquise.moore@gmail.com>
2022-09-30 19:13:17 -0500

    Added username and user pickprompts and a user choice function.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 "use strict";
 
 const choices = ["Rock", "Paper", "Scissors"];
+const userName = prompt("What's your name User?");
+const userPick = prompt("Choose your weapon: Rock, Paper, or Scissors.");
 
 const getComputerChoice = function (arr) {
   const cpuChoice = Math.floor(Math.random() * arr.length);
   const choice = arr[cpuChoice];
 
-  console.log(choice);
+  console.log(`CPU: ${choice}`);
+  //return choice;
 };
 getComputerChoice(choices);
+
+const getPlayerChoice = function () {
+  console.log(`${userName}: ${userPick}`);
+  //return userPick;
+};
+getPlayerChoice();
