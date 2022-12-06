84fe508a458092bf7fbf6d20269f4496daad7382
Jalen Moore <jalenmarquise.moore@gmail.com>
2022-12-06 16:05:23 -0600

    Erased unnecessary code and added comments

--- script.js
+++ script.js
@@ -1,83 +1,12 @@
 "use strict";
-/*
-// CPU choices and initialized score variables
-const choices = ["rock", "paper", "scissors"];
-let userScore = 0;
-let cpuScore = 0;
 
-// CPU selects from "choices" array at random
-const getComputerChoice = function (arr) {
-  let cpuPicks = Math.floor(Math.random() * arr.length);
-  let cpuChoice = arr[cpuPicks];
-  return cpuChoice;
-};
-
-// Specifies round win/game win messages
-let playerWinRound = "User wins this round!";
-let computerWinRound = "Computer wins this round!";
-let draw = "Draw!";
-let playerWin = "User wins the game! Congratulations!";
-let computerWin = "Computer wins the game! You gotta be better than that!";
-
-// Returns the result of each round
-const playRound = function (userPick, computerSelection) {
-  if (userPick === rock && computerSelection === "scissors") {
-    return playerWinRound;
-  } else if (userPick === scissors && computerSelection === "paper") {
-    return playerWinRound;
-  } else if (userPick === paper && computerSelection === "rock") {
-    return playerWinRound;
-  } else if (userPick === computerSelection) {
-    return draw;
-  } else {
-    return computerWinRound;
-  }
-};
-
-// Game algorithm
-const game = function () {
-  // Rounds go up to 1000, but game ends once user or cpu get 3 wins
-  for (let i = 0; i <= 1000; i++) {
-    let userPick = prompt("Rock, Paper, or Scissors.").toLowerCase();
-    let cpu = getComputerChoice(choices);
-    let roundResult = playRound(userPick, cpu);
-
-    // Logs user/cpu pick and result of each round
-    console.log(`User: ${userPick}`, `CPU: ${cpu}`);
-    console.log(`Round ${i}: ${roundResult}`);
-
-    // Keeps score
-    if (roundResult === playerWinRound) {
-      userScore++;
-    } else if (roundResult === computerWinRound) {
-      cpuScore++;
-    } else {
-      draws++;
-    }
-
-    // Scoreboard
-    console.log(`User score: ${userScore}`);
-    console.log(`CPU score: ${cpuScore}`);
-    console.log(`Draws: ${draws}`);
-
-    // Game winning message
-    if (userScore === 3) {
-      alert(playerWin);
-      break;
-    } else if (cpuScore === 3) {
-      alert(computerWin);
-      break;
-    }
-  }
-};
-game();
-*/
-
+// User object
 const user = {
   score: 0,
   scoreDisplay: document.querySelector("#user-score"),
 };
 
+// CPU object
 const cpu = {
   score: 0,
   scoreDisplay: document.querySelector("#cpu-score"),
@@ -94,6 +23,7 @@ const lossGIF = document.querySelector("#lossGIF");
 let isGameOver = false;
 let winningScore = 5;
 
+// CPU chooses weapon based on random number corresponding with choices array
 const getComputerChoice = function () {
   const choices = ["rock", "paper", "scissors"];
   let cpuChoice = Math.floor(Math.random() * choices.length);
@@ -101,6 +31,7 @@ const getComputerChoice = function () {
 };
 
 function checkScore() {
+  // Results after User has won
   if (user.score === winningScore) {
     isGameOver = true;
     rock.classList.toggle("hide");
@@ -108,6 +39,8 @@ function checkScore() {
     paper.classList.toggle("hide");
     winGIF.classList.toggle("hide");
     return (results.textContent = "CONGRATS! YOU WON!");
+
+    //Results after CPU has won
   } else if (cpu.score === winningScore) {
     isGameOver = true;
     rock.classList.toggle("hide");
@@ -120,6 +53,7 @@ function checkScore() {
 }
 
 const playRound = function (userPick, cpuPick) {
+  // Checks user/cpu weapon
   if (userPick === cpuPick) {
     results.textContent = "It's a draw!";
   } else if (
@@ -127,16 +61,19 @@ const playRound = function (userPick, cpuPick) {
     (userPick === "paper" && cpuPick === "rock") ||
     (userPick === "scissors" && cpuPick === "paper")
   ) {
+    // User won round
     results.textContent = `YOU WON THAT ROUND! ${userPick.toUpperCase()} BEATS ${cpuPick.toUpperCase()}!`;
     user.score++;
     user.scoreDisplay.textContent = user.score;
   } else {
+    // CPU won round
     results.textContent = `YOU LOST THAT ROUND! ${cpuPick.toUpperCase()} BEATS ${userPick.toUpperCase()}!`;
     cpu.score++;
     cpu.scoreDisplay.textContent = cpu.score;
   }
 };
 
+// Resets game and UI
 function reset() {
   isGameOver = false;
   results.textContent = "CHOOSE YOUR WEAPON!";
